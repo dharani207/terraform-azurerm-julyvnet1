94b6531c39bbd6c5f4886bd321f6565fb1888fe6
ravi kumar <ravilktkumar@gmail.com>
2021-10-28 13:38:02 +0530

    added files

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+  version         = "=2.46.0"
+  subscription_id = "0d3f9a04-eb28-473e-be1b-29f796b18583"
+  client_id       = "0d039219-f55b-4a6c-90a1-17d0d9ce5cca"
+  client_secret   = "db27Q~zDnY5HThsOx1.EF6BwbQCURZVB8ZjOi"
+  tenant_id       = "6c9f0394-6a39-4f20-90bc-b20008d9b116"
+}
+
+
+
+
+
